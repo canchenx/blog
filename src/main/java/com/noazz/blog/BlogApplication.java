df808efb38d021557e5a1b5f72cb34ae0c734d4d
Noazz <2286877194@qq.com>
2022-07-15 10:36:33 +0800

    test

--- src/main/java/com/noazz/blog/BlogApplication.java
+++ src/main/java/com/noazz/blog/BlogApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlogApplication {
-aaa
+
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
